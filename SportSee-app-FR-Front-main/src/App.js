4b4ece8aef9ecdad2cac556abc1495b281a5ec1b
Audmes <102157665+Audmes@users.noreply.github.com>
2024-06-28 18:27:50 +0200

    Commit : 28/06/2024 - Ajout divers fichiers : work in progress

--- SportSee-app-FR-Front-main/src/App.js
+++ SportSee-app-FR-Front-main/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+import Home from "./views/Home";
+import Profile from "./views/Profile";
+import Settings from "./views/Settings";
+import Community from "./views/Community";
+
+/**
+ * Router to render the Header, the Sidebar and the 4 pages of the application
+ *
+ * @category Router
+ * @component
+ * @returns { React.Component } A React component
+ */
+
+const App = () => {
+    return (
+        <BrowserRouter basename="/">
+            <Header />
+            <Sidebar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/profile/:userId" element={<Profile />} />
+                <Route exact path="/settings" element={<Settings />} />
+                <Route exact path="/community" element={<Community />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
